a9f34fccef0169f3b226a6c95585886175317170
MaciejBaj <maciej@t3rn.io>
2022-07-15 10:34:49 +0200

    build: Remove pallet-xcm from direct XBI deps and use DI

--- src/lib.rs
+++ src/lib.rs
@@ -82,10 +82,7 @@ pub mod pallet {
     /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
     pub trait Config:
-        SendTransactionTypes<Call<Self>>
-        + frame_system::Config
-        + pallet_xcm::Config
-        + pallet_balances::Config
+        SendTransactionTypes<Call<Self>> + frame_system::Config + pallet_balances::Config
     {
         /// Because this pallet emits events, it depends on the runtime's definition of an event.
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
