0af6565f782bba5db1d9d35a7b7940c2cc533091
AwesomeIbex <donovan@t3rn.io>
2023-03-01 14:54:31 +0000

    fix: scabi module error when publishing

--- crates/sabi/src/error.rs
+++ crates/sabi/src/error.rs
@@ -1,7 +1,11 @@
 use codec::{Decode, Encode};
 use scale_info::TypeInfo;
-use sp_runtime::{DispatchError, ModuleError, MAX_MODULE_ERROR_ENCODED_SIZE};
+use sp_runtime::{DispatchError, ModuleError};
 use sp_std::fmt::Debug;
+
+/// The number of bytes of the module-specific `error` field defined in [`ModuleError`].
+/// In FRAME, this is the maximum encoded size of a pallet error type.
+pub const MAX_MODULE_ERROR_ENCODED_SIZE: usize = 4;
 
 /// A wrapper providing access to the module INDEX from the runtime. This allows us to generate
 /// error functionality for use in DispatchError.
